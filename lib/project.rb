94b5ca6fbf26c154d172b5eb3d499aa9528e7031
github username <github email address>
2018-06-28 17:15:05 -0400

    Done.

---
+++ lib/project.rb
@@ -0,0 +1,18 @@
+require 'pry'
+require 'backer.rb'
+
+class Project
+
+  attr_accessor :title, :backers
+
+  def initialize(title)
+    @backers = []
+    @title = title
+  end
+
+  def add_backer(backer)
+    @backers << backer
+    backer.backed_projects << self
+  end
+
+end
